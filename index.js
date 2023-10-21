624e02c0a89e4f1327c6bce910d5988121539e2d
Ashantilogan <Ashantilogan2@gmail.com>
2023-10-21 09:54:40 -0400

    first class functions

---
+++ index.js
@@ -0,0 +1,17 @@
+function receivesAFunction(callback) {
+    callback()
+}
+
+function returnsANamedFunction() {
+    function myNamedFunction() {
+    }
+    return myNamedFunction
+}
+
+function returnsAnAnonymousFunction() {
+   return function() {
+   }
+}
+
+
+
